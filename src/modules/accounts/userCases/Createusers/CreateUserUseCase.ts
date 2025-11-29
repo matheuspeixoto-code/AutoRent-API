05c11be60bc587ee3d8fd4704c7d97a73609ae17
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-11-29 19:45:51 -0300

    adicionando criptografia e verificação de email já existente

--- src/modules/accounts/userCases/Createusers/CreateUserUseCase.ts
+++ src/modules/accounts/userCases/Createusers/CreateUserUseCase.ts
@@ -2,6 +2,8 @@ import { inject, injectable } from "tsyringe";
 import { IUsersRepository } from "../../repository/IUsersRepository";
 import { ICreateUserDTO } from "../../dtos/ICreaetUserDTO";
 
+import {hash} from "bcrypt"
+
 @injectable()
 class CreateUserUseCase{
     constructor(
@@ -10,11 +12,17 @@ class CreateUserUseCase{
     ){}
 
     async execute({name,email,driver_license,password}:ICreateUserDTO):Promise<void> {
+        const userAlreadyExists= await this.userRepository.findByEmail({email})
+
+        if(!userAlreadyExists){
+            throw new Error("User alread exists")
+        }
+        const passwordHash= await hash(password,8)
         await this.userRepository.create({
             name,
             email,
             driver_license,
-            password
+            password: passwordHash
         })
     }
 }
