5b84e5e86a514fa85a166a291056e4b979c0244f
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-05 20:55:16 -0300

    implementando o delete de avatar existente para trocar pelo novo

--- src/modules/accounts/userCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/userCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,6 +1,8 @@
 import { inject, injectable } from "tsyringe";
 import { IUsersRepository } from "../../repository/IUsersRepository";
 
+import { deleteFile } from "../../../../utils/file";
+
 interface IRequest{
     user_id:string;
     avatar_file:string;
@@ -16,6 +18,11 @@ class UpdateUserAvatarUseCase{
 
     async execute({user_id,avatar_file}:IRequest):Promise<void>{
         const user= await this.userRepository.findById(user_id);
+
+        if(user.avatar){
+            await deleteFile(`./tmp/avatar/${user.avatar}`)
+
+        }
 
         user.avatar=avatar_file
 
