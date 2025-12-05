083767553b8385506b5ea16f875b6fdb7f97a68f
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-05 18:41:26 -0300

    criando a migrations de adicionar coluna de avatar na tabela users

---
+++ src/modules/accounts/userCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,14 @@
+
+//Adicionar coluna avatar na tabela users
+//refatoar usuario com a nova coluna
+//configuracao upload multer
+//criar regra de negocio do upload
+//criar controller
+
+class UpdateUserAvatarUseCase{
+    async execute(){
+
+    }
+}
+
+export {UpdateUserAvatarUseCase}
