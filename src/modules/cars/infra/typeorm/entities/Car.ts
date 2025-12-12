2ba973f905f42910a797d8bfbcb5d32b6663ca95
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-12 16:56:17 -0300

    modificando a entidate Car

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,22 +1,47 @@
 import {v4 as uuidV4} from "uuid"
+import{Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn} from "typeorm"
+import { Category } from "./Category";
 
+@Entity("cars")
 class Car{
+    @PrimaryColumn()
     id:string;
+
+    @Column()
     name:string;
+
+    @Column()
     description:string;
+
+    @Column()
     daily_rate:number;
-    available:boolean;
+
+    @Column()
+    available=true;
+
+    @Column()
     license_plate:string;
+
+    @Column()
     fine_amount:number;
+
+    @Column()
     brand:string;
+
+    @CreateDateColumn()
     created_at:Date;
+
+    @ManyToOne(()=>Category)
+    @JoinColumn({name:"category_id"})
+    category:Category
+
+    @Column()
     category_id:string;
 
     constructor(){
         if(!this.id){
             this.id=uuidV4();
-            this.available=true
-            this.created_at=new Date()
+    
         }
     }
 }
