8da856af13dbbe7479a7d1acda5b9d11396e05f3
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-18 17:08:04 -0300

    iniciando com as imagens do carro, criando seu repository,entidade e migrations

---
+++ src/modules/cars/infra/typeorm/repositories/CarsImageRepository.ts
@@ -0,0 +1,29 @@
+import { ICarsImageRepository } from "@modules/cars/repository/implementations/ICarsImageRepository";
+import { CarImage } from "../entities/CarImage";
+import { Repository } from "typeorm";
+import { AppDataSource } from "@data";
+
+
+class CarsImageRepository implements ICarsImageRepository{
+    private repository:Repository<CarImage>
+
+    constructor(){
+        this.repository=AppDataSource.getRepository(CarImage)
+    }
+
+    async create(car_id: string, image_name: string): Promise<CarImage> {
+        const carImage = this.repository.create({
+            car_id,
+            image_name
+        })
+
+        await this.repository.save(carImage)
+
+        return carImage
+        
+    }
+
+
+}
+
+export {CarsImageRepository}
