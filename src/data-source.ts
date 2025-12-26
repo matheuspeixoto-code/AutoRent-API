dabec268a0654bdff8eb6f15862494e6a856aba2
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-25 21:49:42 -0300

    refatorando a conexao com o banco de dados

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,12 +1,13 @@
 import { DataSource } from "typeorm";
+import "dotenv/config"
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "database_ignite",
-  port: 5432,
-  username: "docker",
-  password: "ignite",
-  database: "autorent",
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
 
   entities: ["src/modules/**/entities/*.ts"],
   migrations: ["src/shared/infra/database/migrations/*.ts"],
