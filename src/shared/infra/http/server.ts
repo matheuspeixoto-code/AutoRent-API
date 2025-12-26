ec60e022917027547a64cca4d1527a2d9196d294
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-25 21:50:13 -0300

    refatorando a inicialização do servidor

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,32 +1,6 @@
-import "reflect-metadata";
-import express, { NextFunction, Request, request, Response, response } from "express";
-import { router } from "@shared/infra/http/routes"; 
-import swaggerUI from "swagger-ui-express";
-import swaggerFile from "../../../swagger.json"
-
 import { AppDataSource } from "@data";
-
-import "@shared/container";
-import { AppError } from "@shared/errors/AppError";
-
-const app =express();
-app.use(express.json());
-
-app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerFile))
-
-app.use(router)
-
-app.use((err:Error,request:Request,response:Response,next:NextFunction)=>{
-    if(err instanceof AppError){
-        return response.status(err.statusCode).json({message:err.message})
-    }
-
-    return response.status(500).json({
-        status:"error",
-        message:`nternal server error - ${err.message}`
-
-    })
-})
+import "dotenv/config"
+import { app } from "./app";
 
 AppDataSource.initialize().then(()=>{
     console.log("APP data inicializado")
