5c7fca32c7be509dcef8a9148e56602697ebf09e
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-01-07 21:45:20 -0300

    inserindo a rota de refreshToken

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "@modules/accounts/userCases/authenticateuser/AuthenticateUserController";
+import { RefreshTokenContreller } from "@modules/accounts/userCases/refreshToken/RefreshTokenController";
 
 const authenticateRoutes=Router();
 const authenticateUserController = new AuthenticateUserController()
+const refreshTokenContreller = new RefreshTokenContreller()
 
 authenticateRoutes.post("/sessions",authenticateUserController.handle)
+authenticateRoutes.post("/refresh-token",refreshTokenContreller.handle)
 
 export {authenticateRoutes}
