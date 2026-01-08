efe196049628c96d501831dc825b3261408ead9d
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-01-07 21:35:42 -0300

    configurações de tokens

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default{
+    secret_token:"27aaf7525c13ff8ed6c5e1eaabf32bdb",
+    expires_in_token:"15m" as const,
+    secret_refresh_token:"b47d71df63bafe5847d4554fe9946cc0",
+    expires_refresh_token:"30d" as const,
+    expires_refresh_token_date:30
+}
