cac31c3117bd885159a63175683c3cfe1bbf4026
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-01-07 21:47:16 -0300

    insenrindo, na interface, o metodo ue retornar a data futura de acordo com os dias

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -6,6 +6,9 @@ import utc from "dayjs/plugin/utc";
 dayjs.extend(utc)
 
 class DayjsDateProvider implements IDateProvider{
+    addDate(days: number):Date {
+        return dayjs().add(days,"days").toDate()
+    }
 
     compareInHours(start_date: Date, end_date: Date): number {
         const end_date_utc = this.convertToUTC(end_date)
