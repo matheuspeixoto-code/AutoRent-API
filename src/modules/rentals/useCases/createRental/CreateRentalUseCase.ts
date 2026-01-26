362ade24ada5f3c42546829598d3c9836df05826
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-01-26 18:25:28 -0300

    inserindo o update de disponibilidade do carro

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -3,6 +3,7 @@ import {IRentalsRepository} from "@modules/rentals/repository/IRentalsRepository
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { inject, injectable } from "tsyringe";
+import { ICarsRepository } from "@modules/cars/repository/implementations/ICarsRepository";
 
 
 interface IRequest{
@@ -17,7 +18,10 @@ class CreateRentalUseCase{
         @inject("RentalsRepository")
         private rentalsRepository:IRentalsRepository,
         @inject("DayjsDateProvider")
-        private dateProvider:IDateProvider
+        private dateProvider:IDateProvider,
+        @inject("CarsRepository")
+        private carsRepository: ICarsRepository
+
 
     ){}
     async execute({user_id,car_id,expected_return_date}:IRequest):Promise<Rental>{
@@ -47,6 +51,8 @@ class CreateRentalUseCase{
             car_id,
             expected_return_date
         })
+
+        await this.carsRepository.updateAvailable(car_id,false)
 
         return rental
     }
