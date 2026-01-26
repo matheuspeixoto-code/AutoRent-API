683ca929358de9e8607d55317e05e52d7e402009
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-01-26 18:55:14 -0300

    inserindo o metodo para comparar dias

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -5,7 +5,8 @@ interface IDateProvider{
     compareInHours(start_date:Date,end_date:Date):number;
     convertToUTC(date:Date): string;
     dateNow():Date;
-    addDate(days:number):Date
+    addDate(days:number):Date;
+    compareInDate(start_date:Date, end_date:Date): number
 
 }
 
