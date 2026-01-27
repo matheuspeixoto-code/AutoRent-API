34731b4892a0c80c676542c0508c5c37ef7828d2
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-01-27 20:57:29 -0300

    alterando a ordem dos atributos o envio do compareInDate

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -3,7 +3,7 @@ import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repository/IRentalsRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
 interface IRequest{
     id:string;
@@ -11,9 +11,11 @@ interface IRequest{
 }
 
 
+
+@injectable()
 class DevolutionRentalUseCase{
     constructor(
-        @inject("RentalRepository")
+        @inject("RentalsRepository")
         private rentalRepository:IRentalsRepository,
         @inject("CarsRepository")
         private carsRepository: ICarsRepository,
@@ -23,7 +25,7 @@ class DevolutionRentalUseCase{
 
     async execute({id,user_id}:IRequest): Promise<Rental>{
         const rental = await this.rentalRepository.findById(id)
-        const car = await this.carsRepository.findById(id)
+        const car = await this.carsRepository.findById(rental.car_id)
         const minimum_date = 1
 
         if(!rental){
@@ -42,7 +44,7 @@ class DevolutionRentalUseCase{
         }
 
         const delay=this.dateProvider.compareInDate(
-            dateNow, rental.expected_return_date
+            rental.expected_return_date,dateNow
         )
 
         let total =0
