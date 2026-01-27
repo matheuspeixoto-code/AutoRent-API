002aab31e39378a55287a7f26b96bcf91466349e
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-01-27 20:57:51 -0300

    inserindo a rota de devolution

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,13 +1,20 @@
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { Router } from "express";
 import { ensureAutheticated } from "../middlewares/ensureAutheticated";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
 
 const rentalRoute = Router()
 const createRentalController = new CreateRentalController()
+const devolutionRentalController = new DevolutionRentalController()
 
 rentalRoute.post("/",
     ensureAutheticated,
     createRentalController.handle
+)
+
+rentalRoute.post("/devolution/:id",
+    ensureAutheticated,
+    devolutionRentalController.handle
 )
 
 export {rentalRoute}
