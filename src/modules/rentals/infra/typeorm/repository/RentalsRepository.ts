3d6863b842b6b90117c48e2e97e3f12dbd4327bf
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-01-27 20:54:35 -0300

    refatorando o repositorio de rentals

--- src/modules/rentals/infra/typeorm/repository/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repository/RentalsRepository.ts
@@ -1,7 +1,7 @@
 import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
 import { IRentalsRepository } from "@modules/rentals/repository/IRentalsRepository";
 import { Rental } from "../entities/Rental";
-import { Repository } from "typeorm";
+import { IsNull, Repository } from "typeorm";
 import { AppDataSource } from "@data";
 
 
@@ -20,20 +20,33 @@ class RentalsRepository implements IRentalsRepository{
     }
 
     async findOpenRentalByCar(car_id: string): Promise<Rental> {
-        const openByCar = await this.repository.findOne({where:{car_id}})
+        const openByCar = await this.repository.findOne({
+            where:{
+                car_id,
+                end_date:IsNull()
+            }
+        })
 
         return openByCar
     }
     async findOpenRentalByUser(user_id: string): Promise<Rental> {
-        const openByUser = await this.repository.findOne({where:{user_id}})
+        const openByUser = await this.repository.findOne({
+            where:{
+                user_id,
+                end_date:IsNull()
+            }
+        })
 
         return openByUser
     }
-    async create({car_id,user_id,expected_return_date}: ICreateRentalDTO): Promise<Rental> {
+    async create({car_id,user_id,expected_return_date,id,end_date,total}: ICreateRentalDTO): Promise<Rental> {
         const rental = this.repository.create({
             car_id,
             expected_return_date,
-            user_id
+            user_id,
+            id,
+            end_date,
+            total
         })
 
         await this.repository.save(rental)
