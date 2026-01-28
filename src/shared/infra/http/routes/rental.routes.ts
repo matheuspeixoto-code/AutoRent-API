54ddb9043e99bfdb875e9bbe3f48b527c0143cf3
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-01-27 21:25:40 -0300

    inserindo a rota liostagem de rentals

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -2,10 +2,12 @@ import { CreateRentalController } from "@modules/rentals/useCases/createRental/C
 import { Router } from "express";
 import { ensureAutheticated } from "../middlewares/ensureAutheticated";
 import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListRentalByUserController } from "@modules/rentals/useCases/listRentalByUser/ListRentalByUserController";
 
 const rentalRoute = Router()
 const createRentalController = new CreateRentalController()
 const devolutionRentalController = new DevolutionRentalController()
+const listRentalByUserController = new ListRentalByUserController()
 
 rentalRoute.post("/",
     ensureAutheticated,
@@ -15,6 +17,11 @@ rentalRoute.post("/",
 rentalRoute.post("/devolution/:id",
     ensureAutheticated,
     devolutionRentalController.handle
+)
+
+rentalRoute.get("/user",
+    ensureAutheticated,
+    listRentalByUserController.handle
 )
 
 export {rentalRoute}
