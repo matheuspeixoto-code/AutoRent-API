21655eb834955ddaeaa945664b519eec2afe18de
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-01-29 20:55:23 -0300

    inserindo o env do banco de dados de teste

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ export const AppDataSource = new DataSource({
   port: Number(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  database: process.env.NODE_ENV === "test" ? process.env.POSTGRES_DB_TEST : process.env.POSTGRES_DB,
 
   entities: ["src/modules/**/entities/*.ts"],
   migrations: ["src/shared/infra/database/migrations/*.ts"],
