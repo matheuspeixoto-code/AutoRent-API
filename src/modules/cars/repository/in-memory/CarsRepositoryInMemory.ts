2205b48871c4d8786b4fbfc2ae56d1c142f62bde
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-01-29 20:59:38 -0300

    reorganizando o CarsRepositoryInMemory

--- src/modules/cars/repository/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repository/in-memory/CarsRepositoryInMemory.ts
@@ -4,12 +4,12 @@ import { Car } from "@modules/cars/infra/typeorm/entities/Car"
 
 
 class CarsRepositoryInMemory implements ICarsRepository{
+    cars: Car[]= []
    async updateAvailable(id: string, available: boolean): Promise<void> {
         const findIndex = this.cars.findIndex(car =>car.id === id)
         this.cars[findIndex].available = available
     }
     
-    cars: Car[]= []
     async create({
         name,
         description,
