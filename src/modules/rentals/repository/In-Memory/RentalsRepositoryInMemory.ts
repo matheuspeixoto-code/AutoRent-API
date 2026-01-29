15a868e29eb5f3472b287d74e176ae0d3852b848
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-01-29 20:58:42 -0300

    refatorando o repository de RentalInMemory

--- src/modules/rentals/repository/In-Memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repository/In-Memory/RentalsRepositoryInMemory.ts
@@ -1,32 +1,46 @@
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "../IRentalsRepository";
 import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
+import { v4 as uuidV4 } from "uuid";
 
+class RentalsRepositoryInMemory implements IRentalsRepository {
+    rentals: Rental[] = [];
 
-
-class RentalsRepositoryInMemory implements IRentalsRepository{
-    rentals :Rental[] = []
     async findOpenRentalByCar(car_id: string): Promise<Rental> {
-        return this.rentals.find(rental => rental.car_id===car_id && !rental.end_date)
+        return this.rentals.find(
+            rental => rental.car_id === car_id && rental.end_date === null
+        );
     }
+
     async findOpenRentalByUser(user_id: string): Promise<Rental> {
-        return this.rentals.find(rental => rental.user_id===user_id && !rental.end_date)
+        return this.rentals.find(
+            rental => rental.user_id === user_id && rental.end_date === null
+        );
     }
-    
-    async create({user_id,car_id,expected_return_date}: ICreateRentalDTO): Promise<Rental> {
 
+    async create({ user_id, car_id, expected_return_date }: ICreateRentalDTO): Promise<Rental> {
         const rental = new Rental();
-        Object.assign(rental,{
+
+        Object.assign(rental, {
+            id: uuidV4(),
             car_id,
-            expected_return_date,
             user_id,
-            start_date:new Date()
-        })
+            expected_return_date,
+            start_date: new Date(),
+            end_date: null,
+        });
 
-        this.rentals.push(rental)
+        this.rentals.push(rental);
+        return rental;
+    }
 
-        return rental
+    async findById(id: string): Promise<Rental> {
+        return this.rentals.find(rental => rental.id === id);
+    }
+
+    async findByUser(user_id: string): Promise<Rental[]> {
+        return this.rentals.filter(rental => rental.user_id === user_id);
     }
 }
 
-export {RentalsRepositoryInMemory}
+export { RentalsRepositoryInMemory };
