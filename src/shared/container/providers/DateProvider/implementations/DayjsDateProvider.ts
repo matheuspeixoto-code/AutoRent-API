311a80690fe81ecbaa23d658a7c37ca5a6b54230
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-02-02 14:19:43 -0300

    implementando a funcao de retornar data por hora,na expiracao

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -6,6 +6,9 @@ import utc from "dayjs/plugin/utc";
 dayjs.extend(utc)
 
 class DayjsDateProvider implements IDateProvider{
+    addHours(hours: number): Date {
+        return dayjs().add(hours,"hour").toDate()
+    }
     compareInDate(start_date: Date, end_date: Date): number {
         const end_date_utc = this.convertToUTC(end_date)
         const start_end_utc = this.convertToUTC(start_date)
