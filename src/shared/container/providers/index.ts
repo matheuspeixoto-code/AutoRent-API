fef2a980a3b255be0416e20ec0c85ffea32d8ce5
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-02-02 14:21:11 -0300

    inserindo as dependecias do provider de Emails

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,8 +2,16 @@ import { container } from "tsyringe";
 
 import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
+import { IMailProvider } from "./MailProvider/IMailProvider";
+import { EtherealIMailProvider } from "./MailProvider/implementations/EtherealIMailProvider";
 
 container.registerSingleton<IDateProvider>(
     "DayjsDateProvider",
     DayjsDateProvider
+)
+
+
+container.registerInstance<IMailProvider>(
+    "EtherealIMailProvider",
+    new EtherealIMailProvider()
 )
