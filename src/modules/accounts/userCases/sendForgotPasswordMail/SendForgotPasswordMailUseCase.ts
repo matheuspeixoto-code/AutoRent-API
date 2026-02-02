3672fa2d20950a7bcc57eee83585272203bc2c14
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-02-02 15:03:25 -0300

    refatorando para trabalhar com arquivo de estilizacao dos emails

--- src/modules/accounts/userCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/userCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -2,9 +2,12 @@ import { IUsersRepository } from "@modules/accounts/repository/IUsersRepository"
 import { IUsersTokenRepository } from "@modules/accounts/repository/IUsersTokenRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
+
 import { AppError } from "@shared/errors/AppError";
 import { inject, injectable } from "tsyringe";
+
 import {v4 as uuidV4} from "uuid"
+import {resolve} from "path"
 
 
 @injectable()
@@ -22,6 +25,13 @@ class SendForgotPasswordMailUseCase{
     async execute(email:string):Promise<void>{
         const user = await this.userRepository.findByEmail(email)
 
+        const templatePath = resolve(__dirname,
+            "..",
+            "..",
+            "views", 
+            "emails", 
+            "forgotPassword.hbs")
+
         if(!user){
             throw new AppError("User does not exists!")
         }
@@ -29,13 +39,23 @@ class SendForgotPasswordMailUseCase{
         const token = uuidV4()
         const expires_date = this.dateProvider.addHours(3)
 
+        const variables = {
+            name:user.name,
+            link:`${process.env.FORGOT_MAIL_URL}${token}`
+        }
+
         await this.userTokenRepository.create({
             refresh_token:token,
             user_id:user.id,
             expires_date
         })
 
-        await this.mailProvider.sendMail(email,"Recuperacao de senha",`o link para o reset e ${token}`)
+        await this.mailProvider.sendMail(
+            email,
+            "Recuperacao de senha",
+            variables,
+            templatePath
+        )
 
     }
 }
