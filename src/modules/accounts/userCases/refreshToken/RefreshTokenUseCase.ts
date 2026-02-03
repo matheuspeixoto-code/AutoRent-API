6421deaab83bfbf8663798d50b35adf8e59ce3d8
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-02-03 14:38:21 -0300

    refatorando o refresh token para voltar a trabalhar com o token

--- src/modules/accounts/userCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/userCases/refreshToken/RefreshTokenUseCase.ts
@@ -11,6 +11,11 @@ interface IPayload{
 }
 
 
+interface ITokenResponse{
+    token:string;
+    refresh_token:string;
+}
+
 @injectable()
 class RefreshTokenUseCase{
     constructor(
@@ -19,7 +24,7 @@ class RefreshTokenUseCase{
         @inject("DayjsDateProvider")
         private dayjsDateProvader:IDateProvider
     ){}
-    async execute(token:string):Promise<string>{
+    async execute(token:string):Promise<ITokenResponse>{
 
         const {email,sub}=verify(token,auth.secret_refresh_token) as IPayload
         const user_id = sub
@@ -47,7 +52,15 @@ class RefreshTokenUseCase{
             user_id
         })
 
-        return refresh_token
+        const newToken = sign({},auth.secret_token,{
+            subject:user_id,
+            expiresIn:auth.expires_in_token
+        })
+
+        return {
+            refresh_token,
+            token:newToken
+        }
         
     }
 
