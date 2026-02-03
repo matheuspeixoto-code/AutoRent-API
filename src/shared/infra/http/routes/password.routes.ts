bd4d7dedaf841076373e2408f600f13a6b307dd7
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-02-03 14:02:32 -0300

    inserindo a rota de reset senha

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,3 +1,4 @@
+import { ResetPasswordController } from "@modules/accounts/userCases/resetPassword/ResetPasswordController";
 import { SendForgotPasswordMailController } from "@modules/accounts/userCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 
 import { Router } from "express";
@@ -5,8 +6,10 @@ import { Router } from "express";
 const passwordRutes = Router()
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController()
+const resetPasswordController = new ResetPasswordController()
 
 passwordRutes.post("/forgot",sendForgotPasswordMailController.handle)
+passwordRutes.post("/reset",resetPasswordController.handle)
 
 
 export {passwordRutes}
